3939f74a19b018910c35f7e8d0025ca545541dfd
RAMKUMAR S <ramkumarsanadi1@gmail.com>
2024-04-07 22:46:46 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({})],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "jsm-x9",
+      project: "javascript-react",
+    }),
+  ],
 
   build: {
     sourcemap: true,
